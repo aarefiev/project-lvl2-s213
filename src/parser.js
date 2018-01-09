702a0baea8439b6b183ad0fbb6c9f92ba9857da5
Artem Arefiev <a.arefiev@inbox.ru>
2018-01-09 16:08:29 +0400

    Add diff support for INI files

--- src/parser.js
+++ src/parser.js
@@ -3,6 +3,7 @@
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const parser: {
   data: any,
@@ -11,6 +12,7 @@ const parser: {
   data: {
     json: JSON.parse,
     yaml: yaml.safeLoad,
+    ini: ini.parse,
   },
   process: function process(pathToFile: string) {
     const content = fs.readFileSync(pathToFile, 'utf8');
